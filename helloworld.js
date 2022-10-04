d2da39125466e51fe052d4871f437c2e4c0ee3f5
s4n1t4r1um <40229295+s4n1t4r1um@users.noreply.github.com>
2022-10-04 10:12:07 +0200

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello World');
+  res.end('Hello World v2.0');
 });
 server.listen(8080);
